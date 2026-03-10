c84c4b9e95808ab8b02b6a8a2e929b1a0b2e464e
Mohit Kumar <mohitkumarbedwal777@gmail.com>
2026-03-10 21:14:24 +0530

    Delete Test7.java

--- Test7.java
+++
@@ -1,7 +0,0 @@
-public class Test7 {
-    public static void main(String[] args) {
-        Calc obj = new Calc();
-        int result = obj.factorial(5);
-        System.out.println("Factorial = " + result);
-    }
-}

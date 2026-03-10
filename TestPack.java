f56886f109bfabe3ac794aa20abd3d144eda80c0
Mohit Kumar <mohitkumarbedwal777@gmail.com>
2026-03-10 21:15:22 +0530

    Delete TestPack.java

--- TestPack.java
+++
@@ -1,9 +0,0 @@
-public class TestPack {
-    public static void main(String[] args) {
-        Abc obj = new Abc();
-        obj.display1();
-
-        Abc2 obj2 = new Abc2();
-        obj2.display2();
-    }
-}
